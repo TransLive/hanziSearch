38ba06c937986fa948d1c780eb20f37c1922b597
Translive <translivie@gmail.com>
2017-08-24 10:30:46 +0900

    first create

---
+++ chazi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chazi
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
